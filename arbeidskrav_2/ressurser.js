1405916915974a0cee9db42abd19472d43130837
Tore Marius Akerbæk <tore.m.akerbak@hiof.no>
2023-01-09 11:33:54 +0100

    Lag til ressurser-fil i AK2

---
+++ arbeidskrav_2/ressurser.js
@@ -0,0 +1,91 @@
+const resources = [
+    {
+        category: "HTML",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "CSS",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "JavaScript",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "React",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Sanity and headless CMS",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
